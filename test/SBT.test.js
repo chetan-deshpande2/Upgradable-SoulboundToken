f6309ec742ec954d68fc39fc7985c238772fa0a3
Chetan Deshpande <chetan.deshpande@protonmail.com>
2022-12-26 18:50:51 +0530

    :hammer: Added Signature In Test File

--- test/SBT.test.js
+++ test/SBT.test.js
@@ -1,40 +1,84 @@
 const { ethers } = require("hardhat")
 const { expect } = require("chai")
+const { utils, Wallet } = require('ethers')
 const { getSignature } = require('../lib/genrateSig')
 
 const MetaData =
     "https://gateway.pinata.cloud/ipfs/QmcFc5kmpbRvhoQjfUfR2PmUotJQsz3s83rt22529xnvcB";
 
-let Certificate, certificate, singer, addr1, addr2;
+let Certificate, certificate, owner, addr1, addr2;
 
-const passiveAddress = "0x0f6A79A579658E401E0B81c6dde1F2cd51d97176"
-const passivePrivateKey =
-    "0xad54bdeade5537fb0a553190159783e45d02d316a992db05cbed606d3ca36b39";
+const privateKey =
+    "1f5deadd9e3e400c6f1139a9b43e328cecfec331285a89d78eb25a96f77e07b8";
 
 const Name = "Certificate"
 const Symbol = "CTS"
 const Version = "V1"
+let signer
 
 const getLatestBlock = async () => {
     const getCurrentBlock = await ethers.provider.getBlockNumber();
-    return getCurrentBlock
+    const getBlock = (await ethers.provider.getBlock(getCurrentBlock)).timestamp;
+    return getBlock
 }
 const getChainId = async () => {
     const getId = await ethers.provider.getNetwork()
     return getId.chainId
 }
 
+const genrateCompactSigature = async (signer, ...args) => {
+    console.log("===>", args.owner)
+    const signatureValues = await toTypedSignature(...args)
+
+    // const signature = await signer._signTypedData(signatureValues.domain, signatureValues.types, signatureValues.agreement);
+    // const { compact } = utils.splitSignature(signature);
+    // return compact;
+
+}
+
+const toTypedSignature = async (
+    getAddressOfContract, owner, addr2, chainId
+) => {
+    console.log("=============>", getAddressOfContract, owner.address, addr2.address, chainId)
+    const types = {
+        Agreement: [
+            { name: "active", type: "address" },
+            { name: "passive", type: "address" },
+            { name: "tokenURI", type: "string" },
+        ],
+    };
+    const domain = {
+        name: Name,
+        version: Version,
+        chainId: chainId,
+        verifyingContract: getAddressOfContract,
+    };
+
+    const agreement = {
+        active: addr1.address,
+        passive: addr2.address,
+        tokenURI: MetaData,
+    };
+
+    return { types, agreement, domain }
+}
+
+
+
+
 describe("Certifications ", async () => {
     beforeEach(async () => {
-        [singer, addr1, addr2] = await ethers.getSigners();
+        [owner, addr1, addr2] = await ethers.getSigners();
+        signer = new ethers.Wallet(privateKey)
+
         Certificate = await ethers.getContractFactory("Certificate")
         certificate = await Certificate.deploy(Name, Symbol, Version)
     })
 
     describe("Has Properties", () => {
         it("Should Have Correct Name & Symbol", async () => {
-            const getName = await certificate.connect(singer).name()
-            const getSymbol = await certificate.connect(singer).symbol()
+            const getName = await certificate.connect(owner).name()
+            const getSymbol = await certificate.connect(owner).symbol()
             expect(getName).to.equal(Name)
             expect(getSymbol).to.equal(Symbol)
         })
@@ -43,15 +87,15 @@ describe("Certifications ", async () => {
     describe("Give Certificate", () => {
         it("should able to give certificate", async () => {
             const chainId = await getChainId()
-            const getAddressOfContract = (certificate.address).toString()
-            const singerAddress = (singer.address).toString()
-            const getSign = await getSignature(passiveAddress, passivePrivateKey, Name, Version, MetaData, singerAddress, chainId, getAddressOfContract)
+            const getAddressOfContract = certificate.address
+            const args = [getAddressOfContract, chainId, owner, addr2]
+            const getSign = await genrateCompactSigature(signer, ...args)
+            // console.log(getSign)
+
 
         })
 
     })
 
-
-
-
 })
+
